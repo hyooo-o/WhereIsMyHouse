80bd8437d34422136165c16b32c3affaadfd82cf
최지성 <thsrhzn4@naver.com>
2022-11-22 12:17:14 +0900

    sidebar update
    Former-commit-id: 197079ee8e116291d02a1cfad0c01de37e7f1b11

--- front_end/src/views/AppMain.vue
+++ front_end/src/views/AppMain.vue
@@ -1,17 +1,18 @@
 <template>
     <div>
         <the-search></the-search>
-        <!-- <BarChart /> -->
-        <div class="col-sm-12">
-            <div class="row">
-                <div class="col-sm-6 text-center">
-                    <the-card></the-card>
-                </div>
-                <div class="col-sm-6">
-                    <the-news></the-news>
+        <v-container col-sm-8>
+            <div class="col-sm-12">
+                <div class="row">
+                    <div class="col-sm-6 text-center">
+                        <the-card></the-card>
+                    </div>
+                    <div class="col-sm-6">
+                        <the-news></the-news>
+                    </div>
                 </div>
             </div>
-        </div>
+        </v-container>
     </div>
 </template>
 
@@ -19,14 +20,12 @@
 import TheNews from "@/components/main/TheNews.vue";
 import TheSearch from "@/components/main/TheSearch.vue";
 import TheCard from "@/components/main/TheCard.vue";
-// import BarChart from '@/components/chart/BarChart'
 
 export default {
     components: {
         TheNews,
         TheSearch,
         TheCard,
-        // BarChart,
     },
 }
 </script>
